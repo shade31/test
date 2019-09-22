5e42dd9e1e3d78b7777da93ba93a5848ba1f93f3
Shade <Shade25626@gmail.com>
2019-09-22 12:43:09 +0600

    это наш первый комит

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var number = 5;
+var string = "Ansar";
+var sym = Symbol();
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let xyi;
+console.log(xyi);
+
+let person = {
+    name: "Ansar",
+    age: 18,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png','orange.jpeg','apple.bmp'];
+
+console.log(arr[1])
+
+// alert("Hello ANsar");
+
+// let answer = +prompt("ty dalbaeb?", "da");
+
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++, decr--);
+
+console.log(5%2);
+
+console.log("2" === 2);
+
+let isChecked = false;
+let isClose  = false;
+console.log(!isChecked || !isClose);
+
